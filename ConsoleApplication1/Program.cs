cda8d062726fc0d9f6d3862c604b3a980cc2d47b
Lamasd geprol <lamasd69@gmail.com>
2015-02-22 13:14:33 +0100

    Helló világ

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello Világ!!");
+            Console.ReadKey();
         }
     }
 }
